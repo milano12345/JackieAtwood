1349ebcaa73efde2821c5c915269d7e046e7c10c
Jackie Atwood <takalaj@verizon.net>
2019-11-22 19:08:29 -0500

    Structure and components built out

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function NavBar() {
+    return (
+        <div>
+            <a href="/">Welcome</a>
+            <a href="/about">About Me</a>
+            <a href="/work">My Work</a>
+            <a href="/hire">Hire Me</a>
+            <a href="/contact">Contact</a>
+        </div>
+    )
+}
