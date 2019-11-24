1151736f6e521261f9f8e2baba21f2ab79ba57c1
Jackie Atwood <takalaj@verizon.net>
2019-11-24 16:13:52 -0500

    Updated Push, Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,44 @@ export default function Contact() {
     return (
         <div>
             <p>Contact</p>
+            <form>
+                <label>Name: </label>
+                <input
+                    type="text"
+                    placeholder="*Required"
+                />
+                <label>E-mail: </label>
+                 <input
+                    type="text"
+                    placeholder="*Required"
+                />
+                <label>Phone: </label>
+                 <input
+                    type="text"
+                    placeholder="*Required"
+                />
+                <label>Subject: </label>
+                 <input
+                    type="text"
+                    placeholder="*Required"
+                />
+                <label>Your Message: </label>
+                 <textarea
+                    type="text"
+                    placeholder="*Required"
+                />
+                <label>Prefered Method of Contact: </label>
+                <label>E-mail</label>
+                <input
+                    required
+                    type="checkbox"
+                />
+                   <label>Phone</label>
+                <input
+                    required
+                    type="checkbox"
+                />
+            </form>
         </div>
     )
 }
